39c8b310f2d2d92f5edee910c56268adcdd15898
chengnan <chengnan@putao.com>
2018-01-25 17:15:07 +0800

    use user uuid

--- closet.py
+++ closet.py
@@ -112,13 +112,13 @@ class Closet:
         self.IO = IO_Controller(self.door_port,self.speaker_port,self.scale_port,self.screen_port)
 
         self.initItemData()
-
         if self.visualized_camera is not None:
             self.visualization = VisualizeDetection(self.output_queues[self.visualized_camera])
 
 
     def initItemData():
-        id = {'store_sn': 11120011171226001}
+        import utils
+        id = {'store_sn': utils.get_mac_address()}
         response = requests.get("https://www.hihigo.shop/api/v1/updateGoodsInfo",params=id)
         data = response.json()
         result = {}
