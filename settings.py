3c79c4053135dca3aaae3be33efa387e3a15ce15
chengnan <chengnan@putao.com>
2018-02-01 19:33:32 +0800

    debug

--- settings.py
+++ settings.py
@@ -26,8 +26,24 @@ usb_cameras=["/dev/v4l/by-path/pci-0000:00:14.0-usb-0:10:1.0-video-index0",
 "/dev/v4l/by-path/pci-0000:00:14.0-usb-0:8:1.0-video-index0"
 ]
 
-items={}
+# items={}
 
 init_url = "https://www.hihigo.shop/api/v1/updateGoodsInfo"
 
 SAVE_OUTPUT = False
+
+
+items = {
+    '001001': dict(name='农夫山泉矿泉水', price=2.0, weight=575.0),
+    '002004': dict(name='美汁源果粒橙', price=5.0, weight=487.0),
+    '006001': dict(name='红牛', price=8.0, weight=298.0),
+    '007001': dict(name='脉动', price=6.5, weight=546.0),
+    '008001': dict(name='三得利乌龙茶', price=6.0, weight=528.0),
+    '009001': dict(name='冰糖雪梨', price=6.0, weight=549.0),
+    '010001': dict(name='维他柠檬茶', price=7.0, weight=552.0),
+    '002001': dict(name='雪碧听装', price=3.5, weight=343.0),
+    '002002': dict(name='可口可乐听装', price=3.5, weight=354.0),
+    '003001': dict(name='统一阿萨姆奶茶', price=6.5, weight=550.0),
+    '003002': dict(name='小茗同学黄色', price=6.5, weight=546.0),
+    '003003': dict(name='汤达人豚骨面', price=13.5, weight=184.0),
+}
